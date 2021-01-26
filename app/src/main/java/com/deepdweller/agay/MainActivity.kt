bafec7b752a867368b58e2fbabf8608d60e6789e
Denis <neptundr@mail.ru>
2021-01-26 19:55:34 +0700

    message

--- app/src/main/java/com/deepdweller/agay/MainActivity.kt
+++ app/src/main/java/com/deepdweller/agay/MainActivity.kt
@@ -2,14 +2,16 @@ package com.deepdweller.agay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.*
 import android.widget.Button
 import com.deepdweller.agay.data.gline
 import com.deepdweller.agay.data.lline
+import com.deepdweller.agay.data.rline
 
 class
 MainActivity : AppCompatActivity() {
     lateinit var add:Button
+    lateinit var sbor:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,13 +19,28 @@ MainActivity : AppCompatActivity() {
 
         myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         add=findViewById(R.id.add_button)
-        fun lvlup(gline:FloatArray, lline:FloatArray){
+        sbor = findViewById(R.id.sbor_button)
+        fun lvlup(gline:FloatArray, lline:FloatArray, rline:FloatArray){
+            rline[0]+=0.05f
             gline[1]+=0.05f
             lline[3]+=0.05f
         }
         add.setOnClickListener{
-            if (gline[1]<1.9f){
-                lvlup(gline, lline)
+            if (gline[1]<1.4f){
+                lvlup(gline, lline, rline)
+                myCanvasView.invalidate()
+            }else{
+                add.visibility = INVISIBLE
+                rline[0]=0f
+                gline[1]=1.3f
+                lline[3]=1.3f
+                myCanvasView.invalidate()
+            }
+            sbor.setOnClickListener {
+                rline[0]=0.4f
+                gline[1]=0.4f
+                lline[3]=0.4f
+                add.visibility = VISIBLE
                 myCanvasView.invalidate()
             }
         }
--- app/src/main/java/com/deepdweller/agay/data.kt
+++ app/src/main/java/com/deepdweller/agay/data.kt
@@ -4,7 +4,7 @@ object data {
     var r = 127
     var g = 127
     var b = 127
-    var rline:FloatArray = floatArrayOf(0f, 0f, 0f, 0f)
+    var rline:FloatArray = floatArrayOf(0.4f, 0f, 0f, 0f)
     var gline:FloatArray = floatArrayOf(0f, 0.4f, 0f, 0f)
     var bline:FloatArray = floatArrayOf(0f, 0f, 0f, 0f)
     var lline:FloatArray = floatArrayOf(0f, 0f, 0f, 0.4f)
