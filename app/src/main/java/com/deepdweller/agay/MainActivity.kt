14bdd7f2c91ad82bc4e644fd9f4b96610cfac7f7
79635 <2007eia2408@gmail.com>
2021-01-26 21:12:53 +0700

    message

--- app/src/main/java/com/deepdweller/agay/MainActivity.kt
+++ app/src/main/java/com/deepdweller/agay/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.deepdweller.agay
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.*
 import android.widget.Button
+import android.widget.TextView
 import com.deepdweller.agay.data.gline
 import com.deepdweller.agay.data.lline
 import com.deepdweller.agay.data.rline
@@ -12,34 +14,40 @@ class
 MainActivity : AppCompatActivity() {
     lateinit var add:Button
     lateinit var sbor:Button
+    lateinit var counter:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myCanvasView : MyCanvasView = findViewById(R.id.myView)
-
         myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         add=findViewById(R.id.add_button)
         sbor = findViewById(R.id.sbor_button)
+        counter = findViewById(R.id.textView)
+        var counterint = 0
         fun lvlup(gline:FloatArray, lline:FloatArray, rline:FloatArray){
             rline[0]+=0.05f
             gline[1]+=0.05f
             lline[3]+=0.05f
         }
         add.setOnClickListener{
-            if (gline[1]<1.4f){
+            counterint++
+            counter.text = counterint.toString()+"/20"
+            if (gline[1]<1.7f){
                 lvlup(gline, lline, rline)
                 myCanvasView.invalidate()
             }else{
                 add.visibility = INVISIBLE
                 rline[0]=0f
-                gline[1]=1.3f
-                lline[3]=1.3f
+                gline[1]=1.6f
+                lline[3]=1.6f
                 myCanvasView.invalidate()
             }
             sbor.setOnClickListener {
-                rline[0]=0.4f
-                gline[1]=0.4f
-                lline[3]=0.4f
+                counterint=0
+                counter.text = "0/20"
+                rline[0]=0.8f
+                gline[1]=0.8f
+                lline[3]=0.8f
                 add.visibility = VISIBLE
                 myCanvasView.invalidate()
             }
