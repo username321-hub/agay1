860b8719bb1550a445801f714878364f882aa840
Denis <neptundr@mail.ru>
2021-01-26 18:21:50 +0700

    message

--- app/src/main/java/com/deepdweller/agay/MainActivity.kt
+++ app/src/main/java/com/deepdweller/agay/MainActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
     lateinit var add:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val myCanvasView = MyCanvasView(this)
         setContentView(myCanvasView)
-        add=findViewById(R.id.add_button)
         myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
--- app/src/main/java/com/deepdweller/agay/MyCanvasView.kt
+++ app/src/main/java/com/deepdweller/agay/MyCanvasView.kt
@@ -1,23 +1,43 @@
 package com.deepdweller.agay
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
+import android.graphics.*
+import android.os.Build
 import android.view.View
+import androidx.annotation.RequiresApi
+import android.graphics.ColorMatrix
 import androidx.core.content.res.ResourcesCompat
+import java.lang.Thread.sleep
 
 class MyCanvasView(context: Context): View(context) {
-    private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+//    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
-        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = BitmapFactory.decodeResource(resources, R.drawable.rostki)
+
+//        extraCanvas.drawColor(backgroundColor)
+//        if (::extraBitmap.isInitialized) extraBitmap.recycle()
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        var r = 127
+        var g = 127
+        var b = 127
+        var cmData:FloatArray = floatArrayOf(
+            0f, 0f, 0f, 0f, 0f,
+            1f, 1f, 0f, 0f, 0f,
+            0f, 1f, 0f, 0f, 0f,
+            0f, 1f, 0f, 0f, 0f)
+        var mColorMatrix = ColorMatrix(cmData)
+        var mfilter = ColorMatrixColorFilter(mColorMatrix)
+        mColorMatrix = ColorMatrix(cmData)
+        var paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.setStyle(Paint.Style.FILL_AND_STROKE)
+        paint.setColorFilter(mfilter)
+        canvas.save()
+        canvas.drawBitmap(extraBitmap, 0f, 0f, paint)
     }
 }
