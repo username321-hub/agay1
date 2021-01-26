8a1ece4c40e5c5421a27f5089cc746f51688cf68
Ardemon22 <akorenyek@gmail.com>
2021-01-26 16:52:07 +0700

    message

--- app/src/main/java/com/deepdweller/agay/MainActivity.kt
+++ app/src/main/java/com/deepdweller/agay/MainActivity.kt
@@ -2,10 +2,16 @@ package com.deepdweller.agay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var add:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        setContentView(myCanvasView)
+        add=findViewById(R.id.add_button)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
---
+++ app/src/main/java/com/deepdweller/agay/MyCanvasView.kt
@@ -0,0 +1,23 @@
+package com.deepdweller.agay
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class MyCanvasView(context: Context): View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+    }
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+}
